6fdd32244f5c43b4251cba231607a192bfb9f0a0
wiwindos <zmzhenya@yandex.ru>
2025-06-19 10:17:17 +0500

    11

--- flake.nix
+++ flake.nix
@@ -1,19 +1,34 @@
 {
-  description = "DeepSeek Telegram bot with reproducible dev shell";
+  description = "DeepSeek Telegram bot – reproducible dev shell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [
-        python311 python311Packages.poetry python311Packages.pytest
-        python311Packages.aiogram
-        python311Packages.pytest_httpx python311Packages.pytest_asyncio python311Packages.aiogram_mock
-        sqlite git
-      ];
-      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-    };
+  inputs = {
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        # ► только инструменты, которые реально нужны из Nix
+        buildInputs = with pkgs; [
+          python311        # интерпретатор
+          poetry           # менеджер зависимостей / виртуалка
+          sqlite           # бинарь sqlite3 (если нужен миграциям)
+          git
+        ];
+
+        # чтобы requests/poetry видели корневые сертификаты
+        SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+
+        # автоматически поднимаем venv + зависимости
+        shellHook = ''
+          if [ ! -d .venv ]; then
+            poetry install
+          fi
+        '';
+      };
+    });
 }
